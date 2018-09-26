19b70af64263daedee15ea94d5b53a028c07eb70
Dan Mosora <dan@stitchdata.com>
2018-09-26 17:15:59 +0000

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-responsys',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting CSV files from Responsys via FTP',
       author='Stitch',
       url='https://singer.io',
